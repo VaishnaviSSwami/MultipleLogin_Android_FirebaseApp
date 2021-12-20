2f5352024530f36079b019c65e35b00a675b0b4c
VaishnaviSSwami <vaishsswami@gmail.com>
2021-12-20 22:29:10 +0530

    signup page added (by vaishnavi)

--- app/src/main/java/com/example/foods/MainActivity.java
+++ app/src/main/java/com/example/foods/MainActivity.java
@@ -11,6 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Thread thread=new Thread() {
+            public void run() {
+                try {
+                    sleep(3000);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent i = new Intent(MainActivity.this,home.class);
+                    startActivity(i);
+
+                }
+            }
+
+        };thread.start();
 
     }
 }

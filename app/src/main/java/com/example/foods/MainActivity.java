c4a62f0c18856646057fadf71e6246f7bfed362d
VaishnaviSSwami <vaishsswami@gmail.com>
2021-12-26 20:28:08 +0530

    splashscreen updated
    optional

--- app/src/main/java/com/example/foods/MainActivity.java
+++ app/src/main/java/com/example/foods/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.foods;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,21 +18,55 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Thread thread=new Thread() {
-            public void run() {
-                try {
-                    sleep(3000);
+    }
 
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    Intent i = new Intent(MainActivity.this,home.class);
-                    startActivity(i);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.sidemenu,menu);
+        return true;
+    }
 
-                }
-            }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id=item.getItemId();
+        if(id==R.id.share)
+        {
+            ApplicationInfo applicationInfo=getApplicationContext().getApplicationInfo();
+            String apkpath=applicationInfo.sourceDir;
+            Intent i=new Intent(Intent.ACTION_SEND);
+            i.setType("application/vnd.android.pacakge-archieve");
+            i.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(new File(apkpath)));
+            startActivity(Intent.createChooser(i,"Share Via"));
 
-        };thread.start();
+        }
+        else if (id==R.id.about)
+        {
+            Intent intent=new Intent(MainActivity.this,about_us.class);
+            startActivity(intent);
+        }
+        else if (id==R.id.gallery)
+        {
+            Intent intent=new Intent(MainActivity.this,gallery.class);
+            startActivity(intent);
+        }
+        else if (id==R.id.chef)
+        {
+            Intent intent=new Intent(MainActivity.this,cheif_login.class);
+            startActivity(intent);
+        }
+        else if (id==R.id.customer)
+        {
+            Intent intent=new Intent(MainActivity.this,customer_login.class);
+            startActivity(intent);
+        }
+        else if (id==R.id.contact_us)
+        {
+            Intent intent=new Intent(MainActivity.this,contact_us.class);
+            startActivity(intent);
+        }
+        return true;
+    }
 
+    public void setContentView(int activity_home) {
     }
 }

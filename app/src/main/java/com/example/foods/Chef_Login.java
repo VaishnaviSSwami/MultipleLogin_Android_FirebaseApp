3d5f09d7a29e376ecff3c319e08b138d2fb79524
VaishnaviSSwami <vaishsswami@gmail.com>
2021-12-29 19:17:50 +0530

    login and signup connected

--- app/src/main/java/com/example/foods/Chef_Login.java
+++ app/src/main/java/com/example/foods/Chef_Login.java
@@ -2,7 +2,10 @@ package com.example.foods;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Chef_Login extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class Chef_Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chef_login);
+        Button btn_reg;
+        btn_reg=findViewById(R.id.btn_reg);
+        btn_reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(Chef_Login.this,ChefRegistration.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/foods/customer_login.java
+++ app/src/main/java/com/example/foods/customer_login.java
@@ -2,7 +2,10 @@ package com.example.foods;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class customer_login extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class customer_login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_login);
+
+        Button btn_reg;
+        btn_reg=findViewById(R.id.btn_reg);
+        btn_reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(customer_login.this,CustomerRegistration.class);
+                startActivity(i);
+            }
+        });
     }
 }

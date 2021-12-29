00154db05ab28d967a3255e7df642a2de20ae66a
VaishnaviSSwami <vaishsswami@gmail.com>
2021-12-29 15:00:45 +0530

    Testing

--- app/src/main/java/com/example/foods/MainActivity.java
+++ app/src/main/java/com/example/foods/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.foods;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
@@ -10,14 +12,25 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.material.navigation.NavigationView;
+
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
+    NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DrawerLayout drawerLayout = findViewById(R.id.drawer);
+        navigationView=findViewById(R.id.navigation_view);
+
+        ActionBarDrawerToggle toggle;
+        toggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.navigation_open,R.string.navigation_close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
 
     @Override

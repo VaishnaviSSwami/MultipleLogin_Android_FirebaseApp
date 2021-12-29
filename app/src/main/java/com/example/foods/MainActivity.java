c22d99ff532b75b6ef434e8cfd1278f1783e28f0
VaishnaviSSwami <vaishsswami@gmail.com>
2021-12-29 18:48:55 +0530

    backend connection

--- app/src/main/java/com/example/foods/MainActivity.java
+++ app/src/main/java/com/example/foods/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
     ActionBarDrawerToggle toggle;
 
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (toggle.onOptionsItemSelected(item)) {
@@ -49,27 +50,41 @@ public class MainActivity extends AppCompatActivity {
                 switch (item.getItemId()) {
                     case R.id.chef:
                         Log.i("Menu_drawer", "Chef is selected ");
+                        Intent i=new Intent(MainActivity.this,Chef_Login.class);
+                        startActivity(i);
                         drawerLayout.closeDrawer(GravityCompat.START);
                         break;
 
                     case R.id.customer:
                         Log.i("Menu_drawer", "Customer is selected ");
+                        Intent intent=new Intent(MainActivity.this,customer_login.class);
+                        startActivity(intent);
                         drawerLayout.closeDrawer(GravityCompat.START);
                         break;
+
                     case R.id.about:
                         Log.i("Menu_drawer", "About is selected ");
+                        Intent intent1=new Intent(MainActivity.this,about_us.class);
+                        startActivity(intent1);
                         drawerLayout.closeDrawer(GravityCompat.START);
                         break;
+
                     case R.id.contact_us:
                         Log.i("Menu_drawer", "Contact_us is selected ");
+                        Intent intent3=new Intent(MainActivity.this,contact_us.class);
+                        startActivity(intent3);
                         drawerLayout.closeDrawer(GravityCompat.START);
                         break;
                     case R.id.gallery:
                         Log.i("Menu_drawer", "Gallery is selected ");
+                        Intent intent4=new Intent(MainActivity.this,gallery.class);
+                        startActivity(intent4);
                         drawerLayout.closeDrawer(GravityCompat.START);
                         break;
                     case R.id.share:
                         Log.i("Menu_drawer", "Share is selected ");
+                        Intent intent5=new Intent(MainActivity.this,Share_Apps.class);
+                        startActivity(intent5);
                         drawerLayout.closeDrawer(GravityCompat.START);
                         break;
 

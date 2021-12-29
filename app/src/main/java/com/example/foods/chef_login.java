85a558c65ab19d06d910163ff10b783f09bcfee2
Belliproject <samvidp2@gmail.com>
2021-12-29 18:03:29 +0530

    typo
    typo

--- app/src/main/java/com/example/foods/chef_login.java
+++ app/src/main/java/com/example/foods/chef_login.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class cheif_login extends AppCompatActivity {
+public class chef_login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cheif_login);
+        setContentView(R.layout.activity_chef_login);
     }
 }

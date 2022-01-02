2a020fdeb58eda8ba7b26f1346c6043e07ab6287
Belliproject <95762929+sankalp125@users.noreply.github.com>
2022-01-02 23:33:13 +0530

    chef dashboarsd and add menu

--- app/src/main/java/com/example/foods/Chef_Dashboard.java
+++ app/src/main/java/com/example/foods/Chef_Dashboard.java
@@ -2,7 +2,10 @@ package com.example.foods;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Chef_Dashboard extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class Chef_Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chef_dashboard);
+        Button btn_reg;
+        btn_reg=findViewById(R.id.btn_ADD_MENU);
+        btn_reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(Chef_Dashboard.this,chef_add_menu.class);
+                startActivity(i);
+            }
+        });
     }
 }

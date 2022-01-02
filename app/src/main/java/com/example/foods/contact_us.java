6093e8b81378c7c0f59cd3415d47454af47e2272
Belliproject <samvidp2@gmail.com>
2022-01-02 18:39:51 +0530

    contact us page
    The Contact us page was added and github Libraries were changed by samvid

--- app/src/main/java/com/example/foods/contact_us.java
+++ app/src/main/java/com/example/foods/contact_us.java
@@ -1,10 +1,18 @@
 package com.example.foods;
 
+import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
+import java.util.Calendar;
 
-import com.google.android.gms.vision.text.Element;
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
 
 public class contact_us extends AppCompatActivity {
 
@@ -12,6 +20,34 @@ public class contact_us extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
-
+        Element adsElement = new Element();
+        View aboutPage = new AboutPage(this)
+                .isRTL(false)
+                .setDescription(" Add descrition about your app")
+                .addItem(new Element().setTitle("Version 1.0"))
+                .addGroup("CONNECT WITH US!")
+                .addEmail(" hfhofficial@gmail.com ")
+                .addWebsite("https://humansforhumanity.online/")
+                .addYoutube("https://www.youtube.com/channel/UC-sdg-p9PACs8FBsZu3wYig")   //Enter your youtube link here (replace with my channel link)
+                .addPlayStore("com.example.foods")   //Replace all this with your package name
+                .addInstagram("https://www.instagram.com/humansforhumanityofficial/")    //Your instagram id
+                .addItem(createCopyright())
+                .create();
+        setContentView(aboutPage);
+    }
+    private Element createCopyright()
+    {
+        Element copyright = new Element();
+        @SuppressLint("DefaultLocale") final String copyrightString = String.format("Copyright %d by Your Name", Calendar.getInstance().get(Calendar.YEAR));
+        copyright.setTitle(copyrightString);
+        // copyright.setIcon(R.mipmap.ic_launcher);
+        copyright.setGravity(Gravity.CENTER);
+        copyright.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(contact_us.this,copyrightString,Toast.LENGTH_SHORT).show();
+            }
+        });
+        return copyright;
     }
 }

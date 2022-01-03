ea588f791f58c9f82176ea121bafe8c2f35e8718
Belliproject <samvidp2@gmail.com>
2022-01-03 16:52:11 +0530

    View Menu
    View menu Added

--- app/src/main/java/com/example/foods/chef_add_menu.java
+++ app/src/main/java/com/example/foods/chef_add_menu.java
@@ -2,13 +2,40 @@ package com.example.foods;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class chef_add_menu extends AppCompatActivity {
-
+    private EditText name_of_food, desco , quanto, prize;
+    private Button btn_ADD_MENU_submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chef_add_menu);
+                name_of_food = findViewById(R.id.name_of_food);
+                desco = findViewById(R.id.desco);
+                quanto = findViewById(R.id.quanto);
+                prize = findViewById(R.id.prize);
+        btn_ADD_MENU_submit = findViewById(R.id.btn_ADD_MENU_submit);
+        btn_ADD_MENU_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String cheffood = name_of_food.getText().toString();
+                String chefdesc = desco.getText().toString();
+                String chefquant = quanto.getText().toString();
+                String foodprize = prize.getText().toString();
+                Intent i4 = new Intent(chef_add_menu.this,chef_view_menu.class);
+
+                i4.putExtra("Foodkanam",cheffood);
+                i4.putExtra("desckanam",chefdesc);
+                i4.putExtra("quantitykanam",chefquant);
+                i4.putExtra("pricekanam",foodprize);
+                startActivity(i4);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/foods/chef_view_menu.java
+++ app/src/main/java/com/example/foods/chef_view_menu.java
@@ -3,12 +3,32 @@ package com.example.foods;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class chef_view_menu extends AppCompatActivity {
+
+    private TextView food_name, desc_name, quantity_value, prize_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chef_view_menu);
+        food_name = findViewById(R.id.food_name);
+        desc_name = findViewById(R.id.desc_name);
+        quantity_value = findViewById(R.id.quantity_value);
+        prize_name = findViewById(R.id.prize_name);
+
+
+        String  cheffood = getIntent().getStringExtra("Foodkanam");
+        String chefdesc = getIntent().getStringExtra("desckanam");
+        String chefquant = getIntent().getStringExtra("quantitykanam");
+        String foodprize = getIntent().getStringExtra("pricekanam");
+
+        food_name.setText(cheffood);
+        desc_name.setText(chefdesc);
+        quantity_value.setText(chefquant);
+        prize_name.setText(foodprize);
+
     }
 }

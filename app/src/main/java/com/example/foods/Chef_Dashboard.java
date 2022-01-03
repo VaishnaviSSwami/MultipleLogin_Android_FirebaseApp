a2512c087ecb0fa8df8ed2a5c8ee0e7e2be1511b
Belliproject <95762929+sankalp125@users.noreply.github.com>
2022-01-03 16:07:17 +0530

    some changes of variable declaratoin

--- app/src/main/java/com/example/foods/Chef_Dashboard.java
+++ app/src/main/java/com/example/foods/Chef_Dashboard.java
@@ -6,8 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class Chef_Dashboard extends AppCompatActivity {
+    ImageView food_image;
+    Button Btn_ADD_MENU,Btn_VIEW_MENU;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

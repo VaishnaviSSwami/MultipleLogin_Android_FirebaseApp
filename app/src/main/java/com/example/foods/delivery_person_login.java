a2d2ebec20c631f9d63498be49ecad01519492da
Belliproject <95762929+sankalp125@users.noreply.github.com>
2022-01-11 23:59:20 +0530

    otp varification done

--- app/src/main/java/com/example/foods/delivery_person_login.java
+++ app/src/main/java/com/example/foods/delivery_person_login.java
@@ -1,5 +1,6 @@
 package com.example.foods;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,11 +8,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class delivery_person_login extends AppCompatActivity {
     EditText phone_no;
     Button btn_otp;
+    ProgressBar progressBar_sending_otp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +28,51 @@ public class delivery_person_login extends AppCompatActivity {
         setContentView(R.layout.activity_delivery_person_login);
         Button btn_otp;
         btn_otp=findViewById(R.id.btn_otp);
+        progressBar_sending_otp =findViewById(R.id.progressBar_sending_otp);
         btn_otp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                if(!phone_no.getText().toString().trim().isEmpty()){
                    if((phone_no.getText().toString().trim()).length()==10){
-                       Intent i=new Intent(getApplicationContext(),delivery_person_otp_verification.class);
+
+                       progressBar_sending_otp.setVisibility(View.VISIBLE);
+                       btn_otp.setVisibility(View.INVISIBLE);
+
+                       PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                               "+91" + phone_no.getText().toString(),
+                               10,
+                               TimeUnit.SECONDS,
+                               delivery_person_login.this,
+                               new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                                   @Override
+                                   public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+
+                                       progressBar_sending_otp.setVisibility(View.GONE);
+                                       btn_otp.setVisibility(View.VISIBLE);
+                                   }
+
+                                   @Override
+                                   public void onVerificationFailed(@NonNull FirebaseException e) {
+                                       progressBar_sending_otp.setVisibility(View.GONE);
+                                       btn_otp.setVisibility(View.VISIBLE);
+                                       Toast.makeText(delivery_person_login.this, "Please check Your Internet Connection", Toast.LENGTH_SHORT).show();
+
+                                   }
+
+                                   @Override
+                                   public void onCodeSent(@NonNull String databse_otp, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                                       progressBar_sending_otp.setVisibility(View.GONE);
+                                       btn_otp.setVisibility(View.VISIBLE);
+                                       Intent i=new Intent(getApplicationContext(),delivery_person_otp_verification.class);
+                                       getIntent().putExtra("mobile",phone_no.getText().toString());
+                                       getIntent().putExtra("databse_otp",databse_otp);
+                                       startActivity(i);
+                                   }
+                               }
+                       );
+                     /*  Intent i=new Intent(getApplicationContext(),delivery_person_otp_verification.class);
                        getIntent().putExtra("mobile",phone_no.getText().toString());
-                       startActivity(i);
+                       startActivity(i);*/
                        Toast.makeText(delivery_person_login.this, "OTP SENT SUCCESSFULLY", Toast.LENGTH_SHORT).show();
                    }
                    else{
--- app/src/main/java/com/example/foods/delivery_person_otp_verification.java
+++ app/src/main/java/com/example/foods/delivery_person_otp_verification.java
@@ -1,15 +1,28 @@
 package com.example.foods;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class delivery_person_otp_verification extends AppCompatActivity {
     TextView phone_no_show_otp;
@@ -19,6 +32,7 @@ public class delivery_person_otp_verification extends AppCompatActivity {
     EditText otp4;
     TextView resend_otp;
     Button btn_otp_submit;
+    String getotpbackend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +49,47 @@ public class delivery_person_otp_verification extends AppCompatActivity {
         textView.setText(String.format(
                 "+91-%s",getIntent().getStringExtra("mobile")
         ));
+
+        getotpbackend=getIntent().getStringExtra("database_otp");
+        final ProgressBar progressBar_verify_otp =findViewById(R.id.progressBar_verify_otp);
        btn_otp_submit.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
-               if (!otp1.getText().toString().trim().isEmpty()){
-                   Toast.makeText(delivery_person_otp_verification.this, "OTP Verified Successfully", Toast.LENGTH_SHORT).show();
+               if (!otp1.getText().toString().trim().isEmpty()  && !otp2.getText().toString().trim().isEmpty()  && !otp3.getText().toString().trim().isEmpty()  && otp4.getText().toString().trim().isEmpty()){
+                   String enter_otp =otp1.getText().toString() +
+                           otp2.getText().toString() +
+                           otp3.getText().toString() +
+                           otp4.getText().toString();
+
+                   if (getotpbackend!= null){
+                       progressBar_verify_otp.setVisibility(View.VISIBLE);
+                       btn_otp_submit.setVisibility(View.INVISIBLE);
+                       PhoneAuthCredential phoneAuthCredential = PhoneAuthProvider.getCredential(
+                               getotpbackend,enter_otp
+                       );
+                       FirebaseAuth.getInstance().signInWithCredential(phoneAuthCredential)
+                               .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                   @Override
+                                   public void onComplete(@NonNull Task<AuthResult> task) {
+                                       progressBar_verify_otp.setVisibility(View.GONE);
+                                       btn_otp_submit.setVisibility(View.VISIBLE);
+
+                                       if(task.isSuccessful()){
+                                           Intent intent= new Intent(getApplicationContext(),delivery_person_dashboard.class);
+                                           intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                           startActivity(intent);
+                                           Toast.makeText(delivery_person_otp_verification.this, "OTP Verified Successfully", Toast.LENGTH_SHORT).show();
+                                       }
+                                       else{
+                                           Toast.makeText(delivery_person_otp_verification.this, "Enter The Correct OTP", Toast.LENGTH_SHORT).show();
+                                       }
+                                   }
+                               });
+                   }
+                   else{
+                       Toast.makeText(delivery_person_otp_verification.this, "Please Check Your Internet Connection", Toast.LENGTH_SHORT).show();
+                   }
+
                }
                else{
                    Toast.makeText(delivery_person_otp_verification.this, "Please Enter Correct OTP", Toast.LENGTH_SHORT).show();
@@ -47,6 +97,39 @@ public class delivery_person_otp_verification extends AppCompatActivity {
            }
        });
        otpmove();
+
+       resend_otp =findViewById(R.id.resend_otp);
+       resend_otp.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                       "+91" + getIntent().getStringExtra("mobile"),
+                       10,
+                       TimeUnit.SECONDS,
+                       delivery_person_otp_verification.this,
+                       new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                           @Override
+                           public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+
+                           }
+
+                           @Override
+                           public void onVerificationFailed(@NonNull FirebaseException e) {
+
+                               Toast.makeText(delivery_person_otp_verification.this, "Please check Your Internet Connection", Toast.LENGTH_SHORT).show();
+
+                           }
+
+                           @Override
+                           public void onCodeSent(@NonNull String newdatabse_otp, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                              getotpbackend=newdatabse_otp;
+                               Toast.makeText(delivery_person_otp_verification.this, "OTP Sent Successfully", Toast.LENGTH_SHORT).show();
+
+                           }
+                       }
+               );
+           }
+       });
 
 
     }
